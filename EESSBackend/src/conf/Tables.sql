2296fa99b92eef87da31549f0e5973ed4aa969e4
Ejer <Ejer@10.18.137.136>
2014-02-28 09:52:53 +0100

    Merge origin/master
    Conflicts:
    	web/Bankteller/deposit.jsp
    	web/Bankteller/withdraw.jsp

---
+++ EESSBackend/src/conf/Tables.sql
@@ -0,0 +1,40 @@
+
+
+
+
+create table Subject (
+id_subject int primary key not null unique,
+subject_name varchar(30) not null,
+description varchar(255) not null,
+teacher varchar(30),
+pool varchar(1),
+first_elective_round varchar(1)
+);
+
+create table Teacher (
+id_teacher int primary key not null unique,
+teacher_name varchar(30),
+);
+
+create table Student (
+id_student int primary key not null unique,
+student_name varchar(30),
+);
+
+create table subject_teacher (
+teacher_id int,
+subject_id int,
+constraint fk_teacherid foreign key(teacher_id) references Teacher(id_teacher),
+constraint fk_subjectid foreign key(subject_id) references Subject(id_subject),
+primary key(teacher_id, subject_id)
+);
+
+create table subject_student (
+student_id int,
+subject_id int,
+pool varchar(1),
+priority varchar(1),
+constraint fk_studentid foreign key(student_id) references Student(id_student),
+constraint fk_subjectid foreign key(subject_id) references Subject(id_subject),
+primary key(teacher_id, subject_id)
+);
