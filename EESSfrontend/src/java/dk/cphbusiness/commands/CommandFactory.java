a94bbcf9860b5113648498d08a578f1c81de891f
Frederik Olsen <frederikolsen24@hotmail.com>
2014-03-07 10:56:46 +0100

    CommandFactory gøgl

--- EESSfrontend/src/java/dk/cphbusiness/commands/CommandFactory.java
+++ EESSfrontend/src/java/dk/cphbusiness/commands/CommandFactory.java
@@ -1,4 +1,3 @@
-
 package dk.cphbusiness.commands;
 
 import contract.EessInterface;
@@ -25,36 +24,37 @@ public class CommandFactory implements Factory {
     private static Factory instance;
     private Map<String, Command> commands = new HashMap();
     private EessInterface managerBean = lookupManagerBeanRemote();
-    
+
     private CommandFactory() {
         commands.put("main", new TargetCommand("/main.jsp"));
         commands.put("manage_pools", new ViewPoolsPageCommand("/adm_pools.jsp"));
         commands.put("savePriorities", new SavePrioritiesCommand(("/main.jsp")));
         commands.put("toFirstElectionPage", new ToFirstElectionPageCommand(("/first_election.jsp")));
     }
-    
-    public static Factory getInstance(){
-        if(instance == null){
+
+    public static Factory getInstance() {
+        if (instance == null) {
             instance = new CommandFactory();
         }
         return instance;
     }
-    
+
     @Override
     public Command getCommand(String command) {
         return commands.get(command);
     }
-    
+
     @Override
-    public ArrayList<DTOStudent> getStudents(){
+    public ArrayList<DTOStudent> getStudents() {
         return managerBean.getStudents();
     }
-    
+
     @Override
-    public ArrayList<DTOSubject> getSubjects(){
+    public ArrayList<DTOSubject> getSubjects() {
         return managerBean.getSubjects();
     }
-        private EessInterface lookupManagerBeanRemote() {
+
+    private EessInterface lookupManagerBeanRemote() {
         try {
             Context c = new InitialContext();
             return (EessInterface) c.lookup("java:global/EESSBackend/EESSManagerBean!contract.EessInterface");
