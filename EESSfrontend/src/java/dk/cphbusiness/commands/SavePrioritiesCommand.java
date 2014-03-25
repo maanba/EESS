9b66be4ee72799902fbda6735dd26522e5795af9
Frederik Olsen <frederikolsen24@hotmail.com>
2014-03-25 09:43:11 +0100

    Status message added to two commands.

--- EESSfrontend/src/java/dk/cphbusiness/commands/SavePrioritiesCommand.java
+++ EESSfrontend/src/java/dk/cphbusiness/commands/SavePrioritiesCommand.java
@@ -14,7 +14,6 @@ public class SavePrioritiesCommand extends TargetCommand {
 
     @Override
     public String execute(HttpServletRequest request) {
-        // skal gøres at den gemmer pool, fra second election og en slags null værdi i pool fra first election
         Factory instance = CommandFactory.getInstance();
         List<DTOSubject> list = instance.getSubjects();
         String[] checked = request.getParameter("checked").split(";");
@@ -22,57 +21,7 @@ public class SavePrioritiesCommand extends TargetCommand {
         DTOSubject[] firstPriorities = new DTOSubject[2];
         DTOSubject[] secondPriorities = new DTOSubject[2];
 
-//        for (int i = 0; i < list.size(); i++) {
-//            System.out.println(list.get(i));
-//        }
-//        for (int n = 0; n < list.size(); n++) {
-//            String[] check1 = checked[0].split(",");
-//            if (check1[2].equals(1 + "") && firstPriorities[0] == null) {
-//                if ((list.get(n).getId() + "").equals(check1[1])) {
-//                    firstPriorities[0] = list.get(n);
-//                }
-//            } else if (check1[2].equals(2 + "")) {
-//                if ((list.get(n).getId() + "").equals(check1[1])) {
-//                    secondPriorities[0] = list.get(n);
-//                }
-//            }
-//
-//            String[] check2 = checked[1].split(",");
-//            if (check2[2].equals(1 + "")) {
-//                if ((list.get(n).getId() + "").equals(check2[1])) {
-//                    firstPriorities[1] = list.get(n);
-//                }
-//            } else if (check2[2].equals(2 + "")) {
-//                if ((list.get(n).getId() + "").equals(check2[1])) {
-//                    secondPriorities[1] = list.get(n);
-//                }
-//            }
-//
-//            String[] check3 = checked[2].split(",");
-//            if (check3[2].equals(1 + "")) {
-//                if ((list.get(n).getId() + "").equals(check3[1])) {
-//                    firstPriorities[0] = list.get(n);
-//                }
-//            } else if (check3[2].equals(2 + "")) {
-//                if ((list.get(n).getId() + "").equals(check3[1])) {
-//                    secondPriorities[1] = list.get(n);
-//                }
-//            }
-//
-//            String[] check4 = checked[3].split(",");
-//            if (check4[2].equals(1 + "")) {
-//                if ((list.get(n).getId() + "").equals(check4[1])) {
-//                    firstPriorities[1] = list.get(n);
-//                }
-//            } else if (check4[2].equals(2 + "")) {
-//                if ((list.get(n).getId() + "").equals(check4[1])) {
-//                    secondPriorities[1] = list.get(n);
-//                }
-//            }
-//
-//        }
         String[] pairs = request.getParameter("checked").split(";");
-        
         for (String pair : pairs) {
             String[] pairSplit = pair.split(",");
             int priority = Integer.parseInt(pairSplit[2]);
@@ -98,7 +47,8 @@ public class SavePrioritiesCommand extends TargetCommand {
 
         instance.setPriorities(student);
 
+        request.setAttribute("status", "Your selections have been saved."); 
+
         return super.execute(request);
     }
-
 }
